da87603b66493947b230131f2ca0491952eef636
Kyle King <KyleKing@users.noreply.github.com>
2025-08-24 17:38:42 -0600

    feat: support skipping login for local development

--- yak_shears/server/routes.py
+++ yak_shears/server/routes.py
@@ -21,40 +21,48 @@ ROUTES = [
 ]
 
 
+def create_app_without_auth() -> Starlette:
+    """Only used for local development and testing.
+
+    Returns:
+        Starlette: The configured Starlette application
+    """
+    return Starlette(
+        routes=ROUTES,
+        debug=True,
+        exception_handlers={404: not_found},
+    )
+
+
 def create_app() -> Starlette:
     """Create and configure the Starlette application.
 
     Returns:
         Starlette: The configured Starlette application
     """
-    # Create app with auth middleware
-    app = Starlette(
-        routes=ROUTES,
-        debug=True,
-        exception_handlers={404: not_found},
-    )
+    app = create_app_without_auth()
 
-    # Wrap app with auth middleware
     public_paths = {"/", "/favicon.ico", *AUTH_PUBLIC_PATHS}
     app.add_middleware(AuthMiddleware, public_paths=public_paths)
 
     return app
 
 
-def start(host: str = "localhost", port: int = 8080, *, reload: bool = False) -> None:
+def start(host: str = "localhost", port: int = 8080, *, reload: bool = False, no_auth: bool = True) -> None:
     """Run the ASGI server with uvicorn.
 
     Args:
         host: The hostname to bind to
         port: The port to bind to
         reload: Whether to reload the server on code changes
+        no_auth: Turn off auth middleware. Only use for local development and only allowed when reload is also specified
     """
     log(f"Server running at http://{host}:{port}")
 
     if reload:
         log("Auto-reload enabled: Server will restart on code changes")
         uvicorn.run(
-            "yak_shears.server.routes:create_app",
+            "yak_shears.server.routes:create_app_without_auth" if no_auth else "yak_shears.server.routes:create_app",
             host=host,
             port=port,
             reload=True,
@@ -70,10 +78,15 @@ def cli() -> None:
     parser.add_argument("--host", default="localhost", help="Host to bind to")
     parser.add_argument("--port", type=int, default=8080, help="Port to bind to")
     parser.add_argument("--reload", action="store_true", help="Utilize auto-reload")
+    parser.add_argument(
+        "--no-auth",
+        action="store_true",
+        help="Turn off auth middleware. Only use for local development and only allowed when reload is also specified",
+    )
 
     args = parser.parse_args()
 
-    start(host=args.host, port=args.port, reload=args.reload)
+    start(host=args.host, port=args.port, reload=args.reload, no_auth=args.no_auth)
 
 
 if __name__ == "__main__":
