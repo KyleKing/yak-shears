0160387fe5a9e26ad1f1e427f57b2e232b921852
Kyle King <KyleKing@users.noreply.github.com>
2025-09-26 11:34:51 -0600

    feat: download latest codejar

--- scripts/download-assets.sh
+++ scripts/download-assets.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -ex
 
-# Download HTMX version to public directory
-curl -o yak_shears/static/js/htmx.min.js https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js
+HTMX_VERSION=${HTMX_VERSION:-v2.0.7}
+CODEJAR_VERSION=${CODEJAR_VERSION:-4d19174c5a2759a5bf90be26353f7c85715392fa}  # Default to master, set env var for specific version
+
+# Download HTMX
+curl -o "yak_shears/static/js/htmx.min.js" "https://cdn.jsdelivr.net/npm/htmx.org@$HTMX_VERSION/dist/htmx.min.js"
+
+# Download, transpile, and minify CodeJar
+TEMP_DIR=$(mktemp -d)
+curl -o "$TEMP_DIR/codejar.ts" "https://raw.githubusercontent.com/antonmedv/codejar/$CODEJAR_VERSION/codejar.ts"
+npx tsc "$TEMP_DIR/codejar.ts" --outDir "$TEMP_DIR" --target ES2015 --module ES2015 --noEmitOnError
+npx terser "$TEMP_DIR/codejar.js" -o "yak_shears/static/js/codejar.min.js" --compress --mangle
+rm -rf "$TEMP_DIR"
